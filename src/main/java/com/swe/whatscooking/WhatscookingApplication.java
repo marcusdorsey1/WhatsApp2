5be1e1196f7e4e204b00610f5aae2a3c19a06f70
Armando L. Negron <ar.negron@hotmail.com>
2022-03-06 14:52:11 -0500

    Configuration Fix

--- src/main/java/com/swe/whatscooking/WhatscookingApplication.java
+++ src/main/java/com/swe/whatscooking/WhatscookingApplication.java
@@ -1,13 +1,37 @@
 package com.swe.whatscooking;
 
+import com.swe.whatscooking.entity.Joke;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class WhatscookingApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(WhatscookingApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(WhatscookingApplication.class, args);
 	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Joke joke = restTemplate.getForObject(
+					"https://v2.jokeapi.dev/joke/Pun", Joke.class);
+			log.info(joke.toString());
+		};
+	}
+
 
 }
--- src/main/java/com/swe/whatscooking/entity/Joke.java
+++ src/main/java/com/swe/whatscooking/entity/Joke.java
@@ -1,2 +1,60 @@
-package com.swe.whatscooking.entity;public class Joke {
+package com.swe.whatscooking.entity;
+
+public class Joke {
+    private Long id;
+    private String type;
+    private String setup;
+    private String delivery;
+
+    public Joke() {}
+
+    public Joke(Long id, String type, String setup, String punchline) {
+        this.id = id;
+        this.type = type;
+        this.setup = setup;
+        this.delivery = punchline;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSetup() {
+        return setup;
+    }
+
+    public void setSetup(String setup) {
+        this.setup = setup;
+    }
+
+    public String getDelivery() {
+        return delivery;
+    }
+
+    public void setDelivery(String punchline) {
+        this.delivery = punchline;
+    }
+
+    @Override
+    public String toString() {
+        return "Joke{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                ", setup='" + setup + '\'' +
+                ", punchline='" + delivery + '\'' +
+                '}';
+    }
+
 }
