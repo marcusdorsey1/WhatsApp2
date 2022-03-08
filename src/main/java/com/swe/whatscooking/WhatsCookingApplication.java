454eddb94af13be6b1fb1168f9e853a4e697cee8
Armando L. Negron <ar.negron@hotmail.com>
2022-03-08 00:59:24 -0500

    Code cleanup and organized

--- src/main/java/com/swe/whatscooking/WhatsCookingApplication.java
+++ src/main/java/com/swe/whatscooking/WhatsCookingApplication.java
@@ -1,11 +1,8 @@
 package com.swe.whatscooking;
 
-import com.swe.whatscooking.entity.Joke;
-import com.swe.whatscooking.entity.KrogerAPIBody;
 import com.swe.whatscooking.entity.KrogerClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -13,10 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Base64;
@@ -46,28 +39,24 @@ public class WhatsCookingApplication {
 	}
 */
 	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+	public KrogerClient getClientToken(RestTemplate restTemplate) throws Exception {
 		// API Website link https://sv443.net/jokeapi/v2/
+		String clientId = "whatscooking-acf72f4b2dbda6aa726382a398050a863025969198037206606";
+		String clientSecret = "5yRrceH3sR53QNA7AfC9JTp6I4KTYdmVCvDyBxxA";
+		String body = "grant_type=client_credentials&scope=product.compact";
+		byte[] encodedBytes = Base64.getEncoder().encode((clientId + ":" + clientSecret).getBytes());
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		responseHeaders.add("Authorization", "Basic " + new String(encodedBytes));
+//		System.out.println(responseHeaders.toString());
+		HttpEntity<String> request = new HttpEntity<>(body, responseHeaders);
 
-		return args -> {
-			byte[] encodedBytes = Base64.getEncoder().encode("whatscooking-acf72f4b2dbda6aa726382a398050a863025969198037206606:5yRrceH3sR53QNA7AfC9JTp6I4KTYdmVCvDyBxxA".getBytes());
-			HttpHeaders responseHeaders = new HttpHeaders();
-			responseHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-			responseHeaders.add("Authorization", "Basic " + new String(encodedBytes));
-			System.out.println(responseHeaders.toString());
-			KrogerAPIBody objKAPI = new KrogerAPIBody();
-			objKAPI.setData("grant_type=client_credentials&scope=product.compact");
-			MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
-			map.add("grant_type", "client_credentials");
-			map.add("scope", "product.compact");
-			HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, responseHeaders);
+//		System.out.println("Has Body? " + request.hasBody());
+//		System.out.println("Here is the body Body? " + request.getBody());
+		KrogerClient krogerClient = restTemplate.postForObject("https://api.kroger.com/v1/connect/oauth2/token",request, KrogerClient.class);
+		log.info(krogerClient.toString());
 
-//			HttpEntity<> httpEntity = new HttpEntity<>(MediaType.parseMediaType(objKAPI.getData()), responseHeaders);
-//			System.out.println(httpEntity.toString());
-			System.out.println("Has Body? " + request.hasBody());
-			System.out.println("Here is the body Body? " + request.getBody());
-			KrogerClient krogerClient = restTemplate.postForObject("https://api.kroger.com/v1/connect/oauth2/token",request, KrogerClient.class);
-			log.info(krogerClient.toString());
-		};
+		return krogerClient;
 	}
+
 }
