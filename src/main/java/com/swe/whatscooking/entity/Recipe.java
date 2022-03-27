23d9c07c95f3713685b3f41541243105201b082e
Armando L. Negron <ar.negron@hotmail.com>
2022-03-27 16:15:48 -0400

    Added new tables to database, and initial data for testing original endpoint

--- src/main/java/com/swe/whatscooking/entity/Recipe.java
+++ src/main/java/com/swe/whatscooking/entity/Recipe.java
@@ -9,20 +9,26 @@ public class Recipe {
     private Long id;
 
     private String name;
-    private String ingredients;
-    private String process;
+    private String cuisine;
+    private Integer serving_size;
+    private String description;
+    private String image;
 
-    public Recipe(Long id, String name, String ingredients, String process) {
+    public Recipe(Long id, String name, String cuisine, Integer serving_size, String description, String image) {
         this.id = id;
         this.name = name;
-        this.ingredients = ingredients;
-        this.process = process;
+        this.cuisine = cuisine;
+        this.serving_size = serving_size;
+        this.description = description;
+        this.image = image;
     }
 
-    public Recipe(String name, String ingredients, String process) {
+    public Recipe(String name, String cuisine, Integer serving_size, String description, String image) {
         this.name = name;
-        this.ingredients = ingredients;
-        this.process = process;
+        this.cuisine = cuisine;
+        this.serving_size = serving_size;
+        this.description = description;
+        this.image = image;
     }
 
     public Recipe() {
@@ -44,19 +50,35 @@ public class Recipe {
         this.name = name;
     }
 
-    public String getIngredients() {
-        return ingredients;
+    public String getCuisine() {
+        return cuisine;
     }
 
-    public void setIngredients(String ingredients) {
-        this.ingredients = ingredients;
+    public void setCuisine(String cuisine) {
+        this.cuisine = cuisine;
     }
 
-    public String getProcess() {
-        return process;
+    public Integer getServing_size() {
+        return serving_size;
     }
 
-    public void setProcess(String process) {
-        this.process = process;
+    public void setServing_size(Integer serving_size) {
+        this.serving_size = serving_size;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 }
