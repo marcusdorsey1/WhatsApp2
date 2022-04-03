3d41d1b7b33f1d92f92f046c934a3809dd8ba5d6
Armando L. Negron <ar.negron@hotmail.com>
2022-04-03 17:55:06 -0400

    Added test for positive responses

--- src/test/java/com/swe/whatscooking/rest/RecipeControllerTest.java
+++ src/test/java/com/swe/whatscooking/rest/RecipeControllerTest.java
@@ -1,6 +1,7 @@
 package com.swe.whatscooking.rest;
 
 import com.swe.whatscooking.entity.Recipe;
+import com.swe.whatscooking.service.RecipeService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,16 +9,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.net.URI;
+import java.util.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -28,6 +29,9 @@ public class RecipeControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @Autowired
+    private RecipeService recipeService;
 
     @Test
     public void getAllRecipesNoAccess() {
@@ -47,22 +51,86 @@ public class RecipeControllerTest {
 
     @Test
     public void getRecipeByCuisineNoAccess() {
-        Map<String,String> uriVariables = new HashMap<>();
-        uriVariables.put("cuisine", "Latin");
         ResponseEntity<Recipe> response =
-                this.restTemplate.getForEntity("http://localhost:" + port + "/recipes/cuisine", Recipe.class, uriVariables);
+                this.restTemplate.getForEntity("http://localhost:" + port + "/recipes/cuisine", Recipe.class);
 
         assertThat(response.getStatusCode(), equalTo(HttpStatus.UNAUTHORIZED));
     }
 
     @Test
     public void getRecipeByNameNoAccess() {
-        Map<String,String> uriVariables = new HashMap<>();
-        uriVariables.put("name", "Pizza");
         ResponseEntity<Recipe> response =
-                this.restTemplate.getForEntity("http://localhost:" + port + "/recipes/name", Recipe.class, uriVariables);
+                this.restTemplate.getForEntity("http://localhost:" + port + "/recipes/name", Recipe.class);
 
         assertThat(response.getStatusCode(), equalTo(HttpStatus.UNAUTHORIZED));
+    }
+
+    @Test
+    public void getAllRecipes() {
+        // Calling API
+        ResponseEntity<List> response = restTemplate.exchange(
+                URI.create("http://localhost:" + port + "/recipes"),HttpMethod.GET, getHttpHeaders(), List.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(response.getBody().isEmpty(), equalTo(false));
+    }
+
+    @Test
+    public void getRecipeByID() {
+        Recipe recipe = recipeService.retrieveRecipeByID(1L);
+        ResponseEntity<Recipe> response =
+                this.restTemplate.exchange("http://localhost:" + port + "/recipes/1",HttpMethod.GET, getHttpHeaders(), Recipe.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        assertEquals(response.getBody().getId(),recipe.getId());
+        assertEquals(response.getBody().getName(),recipe.getName());
+
+        ResponseEntity<String> response2 =
+                this.restTemplate.exchange("http://localhost:" + port + "/recipes/0",HttpMethod.GET, getHttpHeaders(), String.class);
+
+        assertThat(response2.getStatusCode(), equalTo(HttpStatus.NOT_FOUND));
+        assertEquals(true, response2.hasBody());
+        assertEquals(404, response2.getStatusCodeValue());
+    }
+
+    @Test
+    public void getRecipeByCuisine() {
+        List<Recipe> recipes = recipeService.retrieveRecipeByCuisine("Latin");
+        ResponseEntity<List> response =
+                this.restTemplate.exchange("http://localhost:" + port + "/recipes/cuisine?cuisine=Latin",HttpMethod.GET, getHttpHeaders(), List.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(response.getBody().isEmpty(), equalTo(false));
+
+        ResponseEntity<List> response2 =
+                this.restTemplate.exchange("http://localhost:" + port + "/recipes/cuisine?cuisine=Brazilian",HttpMethod.GET, getHttpHeaders(), List.class);
+
+        assertThat(response2.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(response2.getBody().isEmpty(), equalTo(true));
+    }
+
+    @Test
+    public void getRecipeByName() {
+        ResponseEntity<List> response =
+                this.restTemplate.exchange("http://localhost:" + port + "/recipes/name?name=Pizza",HttpMethod.GET, getHttpHeaders(), List.class);
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(response.getBody().isEmpty(), equalTo(false));
+
+        ResponseEntity<List> response2 =
+                this.restTemplate.exchange("http://localhost:" + port + "/recipes/name?name=Tofu",HttpMethod.GET, getHttpHeaders(), List.class);
+
+        assertThat(response2.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(response2.getBody().isEmpty(), equalTo(true));
+    }
+
+    private static HttpEntity getHttpHeaders(){
+        HttpHeaders responseHeaders = new HttpHeaders();
+        List<MediaType> list = new ArrayList<>();
+        list.add(MediaType.APPLICATION_JSON);
+        responseHeaders.setAccept(list);
+        responseHeaders.setBasicAuth("user", "password");
+        HttpEntity<String> request = new HttpEntity<String>(responseHeaders);
+        return request;
     }
 
 }
